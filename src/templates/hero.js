780dd148d61c628da38ff059c6b14c7ce73b90f3
AdamSiekierski <adam.siekiera@outlook.com>
2019-06-13 14:30:51 +0200

    Created hero

--- src/templates/hero.js
+++ src/templates/hero.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { Text } from 'react-native';
+import BackgroundImage from '../assets/hero.jpg';
+
+const StyledImageBackground = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+`;
 
 const StyledView = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: lightsalmon;
+  padding: 20px;
 `;
 
 const Title = styled.Text`
@@ -15,10 +20,38 @@ const Title = styled.Text`
   font-weight: bold;
 `;
 
-const Hero = () => (
-  <StyledView>
-    <Title>hi people</Title>
-  </StyledView>
-);
+const SubTitle = styled.Text`
+  font-size: 20px;
+  color: white;
+  text-align: center;
+`;
+
+const SearchInput = styled.TextInput`
+  width: 80%;
+  border-bottom-color: white;
+  border-bottom-width: 2px;
+  height: 50px;
+  font-size: 30px;
+  padding: 5px;
+  text-align: center;
+  color: white;
+  margin-top: 20px;
+`;
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <StyledImageBackground source={BackgroundImage}>
+        <StyledView>
+          <Title>PhotoSpace</Title>
+          <SubTitle>
+            Type any space-related phrase and explore the universe of photos
+          </SubTitle>
+          <SearchInput />
+        </StyledView>
+      </StyledImageBackground>
+    );
+  }
+}
 
 export default Hero;
